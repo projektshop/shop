ee24a034ad3cd4ea806a90fe6ea65022665b4135
Maciej <maciej.czypek@gmail.com>
2019-06-07 21:43:41 +0200

    products.php redirection fix

--- products.php
+++ products.php
@@ -5,7 +5,7 @@ session_start();
 
 ?>
 <?php
-if((isset($_SESSION['zalogowany'])) && ($_SESSION['zalogowany'] == true))
+if (!isset($_SESSION['zalogowany']) || ($_SESSION['zalogowany'] !== true))
 {
     header('Location: index2.php');
     exit();
